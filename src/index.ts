a7c2878539c57f0eef20206f88a9e3ef3ea776fa
Real1ty <vavro.matej3@gmail.com>
2025-02-10 13:57:16 +0100

    feat: added actor config and starting validation

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,41 @@
+import { Actor } from 'apify';
 import { Dataset } from 'crawlee';
 import { createCheerioCrawler } from './crawlers/cheerio.js';
 import createPlaywrightCrawler from './crawlers/playwright.js';
 import createRequestQueues from './crawlers/requestQueue.js';
+import { TravelModelType } from './models/model.js';
 import logger from './utils/logger.js';
 
-const main = async () => {
-  try {
-    logger.debug('Starting the scraping process...');
-
-    const { playwrightQueue, cheerioQueue } = await createRequestQueues();
-
-    const playwrightCrawler = await createPlaywrightCrawler(playwrightQueue, cheerioQueue);
-    const cheerioCrawler = await createCheerioCrawler(cheerioQueue, playwrightQueue);
-
-    logger.debug(
-      `playwright queue requests: ${await playwrightCrawler.requestQueue?.getTotalCount()}`,
-    );
-    logger.debug(`cheerio queue requests: ${await cheerioCrawler.requestQueue?.getTotalCount()}`);
-
-    logger.debug('Starting Playwright crawler...');
-    await playwrightCrawler.run();
-    logger.debug('Playwright crawler finished.');
-
-    logger.debug('Starting Cheerio crawler...');
-    await cheerioCrawler.run();
-    logger.debug('Cheerio crawler finished.');
-
-    // Process and export the dataset
-    const data = await Dataset.getData();
-    console.log(`Data amount: ${data.count}`);
-    // await Dataset.exportToCSV('result.csv');
-
-    logger.debug('Data has been exported to result.csv');
-  } catch (error) {
-    logger.error('An error occurred during the scraping process:', error);
+Actor.main(async () => {
+  const input = await Actor.getInput<TravelModelType>();
+  if (!input) {
+    throw new Error('No input provided');
   }
-};
 
-main();
+  const { playwrightQueue, cheerioQueue } = await createRequestQueues();
+
+  const playwrightCrawler = await createPlaywrightCrawler(playwrightQueue, cheerioQueue);
+  const cheerioCrawler = await createCheerioCrawler(cheerioQueue, playwrightQueue);
+
+  logger.debug(
+    `playwright queue requests: ${await playwrightCrawler.requestQueue?.getTotalCount()}`,
+  );
+  logger.debug(`cheerio queue requests: ${await cheerioCrawler.requestQueue?.getTotalCount()}`);
+
+  logger.debug('Starting Playwright crawler...');
+  await playwrightCrawler.run();
+  logger.debug('Playwright crawler finished.');
+
+  logger.debug('Starting Cheerio crawler...');
+  await cheerioCrawler.run();
+  logger.debug('Cheerio crawler finished.');
+
+  // Process and export the dataset
+  const data = await Dataset.getData();
+  console.log(`Data amount: ${data.count}`);
+  // await Dataset.exportToCSV('result.csv');
+
+  logger.debug('Data has been exported to result.csv');
+}).catch((error) => {
+  logger.error('An error occurred during the scraping process:', error);
+});
--- src/models/model.ts
+++ src/models/model.ts
@@ -1,42 +1,13 @@
-import mongoose, { Document, Schema } from 'mongoose';
 import { z } from 'zod';
-import { WebsiteSource } from '../constants/links.js';
 
-// Zod schema for validation
-export const Model = z.object({
-  location: z.string(),
-  sizeM2: z.number(),
-  pricePerMonth: z.number(),
-  rooms: z.string(),
-  address: z.string().optional(),
-  floor: z.string().optional(),
-  amenities: z.array(z.string()).optional(),
-  source: z.string() as z.ZodType<WebsiteSource>,
-  url: z.string().url(),
-  availableFrom: z.string().optional(),
-  deposit: z.number().optional(),
+export const TravelModel = z.object({
+  from: z.string(),
+  to: z.string(),
+  date: z.date(),
 });
 
 // TypeScript type derived from Zod schema
-export type ModelType = z.infer<typeof Model>;
+export type TravelModelType = z.infer<typeof TravelModel>;
 
-// TypeScript interface for HousePrice
-export interface Model extends Document, ModelType {}
-
-// Mongoose schema definition
-const ModelSchema: Schema = new Schema<Model>({
-  location: { type: String, required: true },
-  sizeM2: { type: Number, required: true },
-  pricePerMonth: { type: Number, required: true },
-  rooms: { type: String, required: true },
-  address: { type: String },
-  floor: { type: String },
-  amenities: { type: [String] },
-  source: { type: String, required: true },
-  url: { type: String, required: true },
-  availableFrom: { type: String }, // New optional date parameter
-  deposit: { type: Number }, // New optional deposit parameter
-});
-
-// Mongoose model export
-export const ModelModel = mongoose.model<Model>('Model', ModelSchema);
+// TypeScript interface
+export interface Travel extends Document, TravelModelType {}
